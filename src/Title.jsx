df166eaae5535ae6b1b440956b36c5b9c1a83f1d
Christian Bustos <christianbus37@gmail.com>
2024-08-07 13:04:35 -0300

    terminado componente de Title

--- src/Title.jsx
+++ src/Title.jsx
@@ -4,9 +4,9 @@ const Title = ({title}) => {
 
   
   return (
-    <div>
+    <>
       <h1>{ title }</h1>
-    </div>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HelloWorldApp from './Title';
-import FirstApp from './FirstApp';
 import './styles.css';
 import CounterApp from './CounterApp';
 
